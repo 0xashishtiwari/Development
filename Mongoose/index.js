3fbb82bbfee3d4075220c0e139d0c7910d5e3b1a
0xashishtiwari <ashishinrewa@gmail.com>
2025-05-16 18:29:32 +0530

    Update method in mongooose

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,32 +1,64 @@
 // getting-started.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-main().then(()=>{
-    
+main()
+  .then(() => {
     console.log("Connected to database successfully");
-})
-.catch(err => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
-
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
 
 const userSchema = new mongoose.Schema({
-  name : String,
-  email : String,
-  age : Number
+  name: String,
+  email: String,
+  age: Number,
 });
 
-const User = mongoose.model("User" , userSchema);
+const User = mongoose.model("User", userSchema);
+
+//find method in mongoose
 
 // User.findOne({_id : "6826c23daf7cc3318fb4b4f0"}).then(res => console.log(res)).catch((err)=>{
 //   console.log(err);
 // })
 
-User.find({age : {$gte : 47}}).then(res => console.log(res[0]._id.toString())).catch((err)=>{
+// User.find({age : {$gte : 47}}).then(res => console.log(res[0]._id.toString())).catch((err)=>{
+//   console.log(err);
+// })
+
+// User.findById('6826c23daf7cc3318fb4b4f0').then((res)=>{
+//   console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// })
+
+//       ---------- update method in mongooose-------------
+
+// User.updateOne({name : "adam"} , {age : 35}).then((res)=>{
+//   console.log(res) ;
+//   console.log("User updated sucessfully");
+// }).catch((err)=>{
+//   console.log(err);
+// })
+
+// User.updateMany({age : {$gt : 20}} , {age : 42}).then((res=>console.log(res))).catch(err =>console.log(err));
+
+// ---------------find and update---------------
+
+User.findOneAndUpdate({name : "adam"}, {age : 34} , {new : true}).then((res)=>{
+  console.log(res);
+})
+
+
+User.findByIdAndUpdate('6826c66c29518108534844cf' , {age : 18} , {new : true} ).then((res)=>{
+  console.log(res);
+}).catch((err)=>{
   console.log(err);
 })
+
 
 // User.insertMany([
 // {name : "tarun" , email : "tarun@gmail.com" , age :23},
@@ -36,10 +68,7 @@ User.find({age : {$gte : 47}}).then(res => console.log(res[0]._id.toString())).c
 //   console.log(data);
 // })
 
-
-
 // const user1 = new User({name : "bob" , email : "bob@gmail.com" , age :23});
-
 
 // user1
 // .save()
@@ -49,4 +78,3 @@ User.find({age : {$gte : 47}}).then(res => console.log(res[0]._id.toString())).c
 // .catch((err)=>{
 //   console.log(err);
 // })
-  
