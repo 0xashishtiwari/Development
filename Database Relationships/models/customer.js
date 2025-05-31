26af6d0d91fdd43fec3a2db6fc34a6c0bcd7a0af
0xashishtiwari <ashishinrewa@gmail.com>
2025-05-31 16:30:15 +0530

    studied how one to many deletion works

--- Database Relationships/models/customer.js
+++ Database Relationships/models/customer.js
@@ -23,15 +23,31 @@ const customerSchema = new Schema({
     ]
 });
 
+
+
+// customerSchema.pre("findOneAndDelete" , async () => {
+//     console.log('pre middleware');
+// });
+
+customerSchema.post("findOneAndDelete" , async (customer) => {
+    if(customer.orders.length){
+       let res = await Order.deleteMany({_id : {$in : customer.orders} });
+       console.log(res);
+    }
+    
+});
+
+
+
 const Order = mongoose.model("Order" , orderSchema);
 const Customer = mongoose.model("Customer" , customerSchema);
 
-const findCustomer = async () => {
-    let result = await Customer.find({}).populate('orders');
+// const findCustomer = async () => {
+//     let result = await Customer.find({}).populate('orders');
 
-    console.log(result[0]) ;
-}
-findCustomer();
+//     console.log(result[0]) ;
+// }
+// findCustomer();
 
 
 // const addOrders = async () => {
@@ -48,3 +64,24 @@ findCustomer();
 
 // addOrders();
 
+const addCustomer = async () => {
+    let newCus = new Customer({
+        name : "vijay"
+    })
+    let newOrder = new Order({
+        item : "Burger",
+        price : 230
+    })
+    newCus.orders.push(newOrder);
+    await newOrder.save()
+    await newCus.save()
+    console.log('added new customer');
+}
+// addCustomer();
+
+const delCust = async () => {
+ let data =   await Customer.findByIdAndDelete('683adb5398dc1b91ad0e80bd');
+//  console.log(data);
+}
+
+delCust();
